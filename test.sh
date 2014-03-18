ddc06e0440d2df9ba37b0db1970230ef2406b671
David Coss <David.Coss@stjude.org>
2014-03-18 15:14:58 -0500

    Updated test script.

--- test.sh
+++ test.sh
@@ -5,7 +5,19 @@ cd python
 PYTHONPATH=$PWD python setup.py test
 RETVAL=$?
 cd ..
-if [[ $RETVAL ]];then
+if [[ $RETVAL -ne 0 ]];then
 	echo Python API Test Failed
 	exit $RETVAL
 fi
+
+echo Testing Java API
+cd java
+ant test
+RETVAL=$?
+cd ..
+if [[ $RETVAL -ne 0 ]];then
+	echo Java API Test Failed
+	exit $RETVAL
+fi
+
+echo Unit Test Passed
